8015eafa06ca7024c718a4a80d95c0f38a7573ef
2908731230 <2908731230@qq.com>
2017-10-06 10:46:12 +0800

    haha

---
+++ newworld/views.py
@@ -0,0 +1,6 @@
+#coding:utf-8
+#from django.http import HttpResponse
+from django.shortcuts import render
+def index(request):
+#    return HttpResponse(u"Hello World!大家好! ")
+    return render(request,"index.html",)
